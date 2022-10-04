3585b69da78da7b7694f18d03b303350fb07b07c
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-10-04 17:19:37 +0200

    add delete

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -114,6 +114,8 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::find($id);
+        $comic->delete();
+        return redirect()->route('comics.index');
     }
 }
--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -17,6 +17,7 @@
                     <th scope="col">type</th>
                     <th scope="col">show</th>
                     <th scope="col">edit</th>
+                    <th scope="col">delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -30,6 +31,13 @@
                         <td>{{$comic->type}}</td>
                         <td><a class="btn btn-primary" href="{{route('comics.show', ['comic' => $comic])}}">Show</a></td>
                         <td><a class="btn btn-warning" href="{{route('comics.edit', ['comic' => $comic])}}">Edit</a></td>
+                        <td>
+                            <form action="{{route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit" value="delete">Delete</button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
